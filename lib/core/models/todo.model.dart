2332c8e4d1d057ffee18d3095526802c417fddf5
antonionicolau <antonio.tioypedro1234@gmail.com>
2023-03-19 09:04:20 +0100

    add todo model

--- lib/core/models/todo.model.dart
+++ lib/core/models/todo.model.dart
@@ -4,4 +4,20 @@ class Todo {
   final String about;
 
   Todo({required this.title, required this.subtitle, required this.about});
+
+  factory Todo.fromMap(Map<String, dynamic> data) {
+    return Todo(
+      title: data['title'],
+      subtitle: data['subtitle'],
+      about: data['about'],
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'title': title,
+      'subtitle': subtitle,
+      'about': about,
+    };
+  }
 }
